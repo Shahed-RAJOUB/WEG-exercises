1ee7cf31f3ac354b1c08d557049b8ec72c621a3a
Shahed-Rajoub <shahd-homs@hotmail.com>
2021-02-26 12:08:42 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MenuComponent} from './menu/menu.component';
+import {ListComponent} from './list/list.component';
+import {AddComponent} from './add/add.component';
+import {DetailComponent} from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'add', component: AddComponent},
+  {path: 'detail', component: DetailComponent},
+  { path: '**', component: MenuComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { AddComponent } from './add/add.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListComponent,
+    DetailComponent,
+    AddComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
